afd215dfd951e20bfba11ac36f835ddd6b54f6de
Nicola Ibrahim <nicola123star4@gmail.com>
2022-10-13 12:35:02 +0300

    Add login APIView

--- accounts/views.py
+++ accounts/views.py
@@ -1,43 +1,60 @@
-from django.shortcuts import render, redirect
-from django.views.generic.edit import CreateView
-from django.contrib.auth.models import User
-from . import forms
+
+from accounts import models, services
 from django.contrib.auth import login, authenticate
-from django.http import HttpResponse
 from django.urls import reverse_lazy
 
+from rest_framework import exceptions
+from rest_framework import generics, permissions, views, authentication
+from rest_framework.response import Response
+
+from . import serializers, authenticatons
 
 
 # Create your views here.
-class CustomerSignupView(CreateView):
-    model = User
-    form_class = forms.CustomerSignupForm
-    template_name = "registration/signup.html"
-    success_url = reverse_lazy("/")
+class CustomersListView(generics.ListCreateAPIView):
+    queryset = models.Customer.objects.all()
+    serializer_class = serializers.CustomersSerializer
+    authentication_classes = [authenticatons.CustomAuth]
+    permission_classes = [permissions.IsAuthenticated]
 
-    def form_valid(self, form):
-        print("------------------yes valid-----------------")
-        user = form.save()
-        # Do login
-        # username = form.cleaned_data.get('username')
-        # password = form.cleaned_data.get('password1')
-        # user = authenticate(self.request, username=username, password=password)
-        # login(self.request, user)
-        
+class CustomerRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Customer.objects.all()
+    serializer_class = serializers.CustomersSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
 
-class StaffSignupView(CreateView):
-    model = User
-    form_class = forms.StaffSignupForm
-    template_name = "registration/signup.html"
-    success_url = reverse_lazy("/")
-    
 
-    def form_valid(self, form):
-        print("------------------yes valid-----------------")
-        user = form.save()
-        # Do login
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password1')
-        user = authenticate(self.request, username=username, password=password)
-        login(self.request, user)
 
+class StaffListView(generics.ListCreateAPIView):
+    queryset = models.Staff.objects.all()
+    serializer_class = serializers.StaffSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+
+
+class StaffRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Staff.objects.all()
+    serializer_class = serializers.StaffSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+
+
+class LoginApi(views.APIView):
+    def post(self, request):
+        username = request.data['username']
+        password = request.data['password']
+
+        user = models.User.objects.filter(username=username).first()
+
+        if(user is None):
+            raise exceptions.AuthenticationFailed("Invalid credentials")
+
+        if(not user.check_password(raw_password=password)):
+            raise exceptions.AuthenticationFailed("Invalid credentials")
+
+        token = services.create_token(user.id)
+        response = Response()
+        response.set_cookie(key='jwt', value=token,  httponly=True)
+        return response
