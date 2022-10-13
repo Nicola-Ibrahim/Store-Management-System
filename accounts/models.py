61952bf03b203510e6e6ca86e3f194fac2cbd47b
Nicola Ibrahim <nicola123star4@gmail.com>
2022-10-13 12:32:50 +0300

    Create custom user model with proxy models

--- accounts/models.py
+++ accounts/models.py
@@ -1,39 +1,108 @@
+from email.policy import default
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db.models.signals import post_save
-from django.dispatch import receiver
+from django.core.exceptions import ValidationError
+from django.db.models import Q
 
 
 # Create your models here.
-class Person(models.Model):
 
-    STANDARD = 'STD'
-    MANAGER = 'MGR'
-    SR_MANAGER = 'SRMGR'
-    PRESIDENT = 'PRES'
+class UserManager(BaseUserManager):
+    """
+    Creates and saves a User with the given data.
+    """
+    def create(self, **kwargs):
+        if not kwargs.get('username'):
+            raise ValidationError("Username not given")
+        
+        user = self.model(**kwargs)
+        user.set_password(kwargs.get('password'))
+        user.save(using=self._db)
 
-    EMPLOYEE_TYPES = (
-        (STANDARD, 'base employee'),
-        (MANAGER, 'manager'),
-        (SR_MANAGER, 'senior manager'),
-        (PRESIDENT, 'president')
-    )
+        return user
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def create_superuser(self, username, email, password):
+        """
+        Creates and saves a superuser with the given username, and password.
+        """
+        user = self.create(
+            username=username,
+            email=email,
+            password=password,
+            is_staff=True,
+            is_superuser=True
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+
+class User(AbstractUser):
+
+
+    class Role(models.TextChoices):
+        NONE = 'NONE', 'none'
+        CUSTOMER = 'CUSTOMER', 'customer'
+        STANDARD = 'STD', 'base employee'
+        MANAGER = 'MGR' , 'manager'
+        SR_MANAGER = 'SRMGR', 'senior manager'
+        PRESIDENT = 'PRES', 'president'
+
+    
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+    
+    objects = UserManager()
+
+
     phone_number = models.IntegerField(null=True, blank=True)
     state = models.CharField(max_length=50, null=True, blank=True)
     city = models.CharField(max_length=50, null=True, blank=True)
     street = models.CharField(max_length=50, null=True, blank=True)
     zipcode = models.IntegerField(null=True, blank=True)
-    role = models.CharField(max_length=25, null=True, blank=True, choices=EMPLOYEE_TYPES)
+    role = models.CharField(max_length=25, null=True, blank=True, choices=Role.choices, default=Role.NONE)
     manager_id = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL)
 
-    
-    def __str__(self):
-        return self.user.username
+class CustomerManager(UserManager):
+    def get_queryset(self):
+        result = super().get_queryset()
+        return result.filter(role=User.Role.CUSTOMER)
 
-    def __repr__(self):
-        return self.__str__()
+class Customer(User):
+    class Meta:
+        proxy = True
+
+    objects = CustomerManager()
+       
+    def save(self, *args, **kwargs) -> None:
+        if not self.pk:
+            self.role = User.Role.CUSTOMER
+        return super().save(*args, **kwargs)
+
+
+class StaffManager(UserManager):
+    def get_queryset(self):
+        result = super().get_queryset()
+        return result.filter(~Q(role=User.Role.CUSTOMER))
+
+
+class Staff(User):
+
+    class Meta:
+        proxy = True
+
+    REQUIRED_FIELDS = ['role']
+
+    objects = StaffManager()
+
+    def save(self, *args, **kwargs) -> None:
+        if not self.pk:
+            self.is_staff = True
+        return super().save(*args, **kwargs)
 
 
     
