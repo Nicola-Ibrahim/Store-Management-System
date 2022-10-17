a03ef636a00fa854c1e4ba93b8e33d2bdad84499
Nicola Ibrahim <nicola123star4@gmail.com>
2022-10-17 12:10:52 +0300

    Add models to production

--- production/models.py
+++ production/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+
+    class Meta:
+        verbose_name = ("Item")
+        verbose_name_plural = ("Items")
+
+    def __str__(self):
+        return self.name
+
+ 
