b364fb9c5d8a93ef86de3a424e833eb1d53825c8
Nicola Ibrahim <nicola123star4@gmail.com>
2022-10-17 12:11:46 +0300

    sales app urls

--- sales/urls.py
+++ sales/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
-urlpatterns = [
-
-]
+urlpatterns = format_suffix_patterns(
+    [
+        path("orders/", views.OrdersListCreateView.as_view(), name='orders-list'),
+        path("orders/<int:pk>/", views.OrderRetrieveView.as_view(), name='order-detail'),
+    ]
+)    
+    
