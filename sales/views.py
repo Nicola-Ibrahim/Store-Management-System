b0fbd0a10583501576666a2f3cb628b0ff50f4fa
Nicola Ibrahim <nicola123star4@gmail.com>
2022-10-17 12:12:07 +0300

    sales' views

--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from . import models, serializers
 
 # Create your views here.
+class OrdersListCreateView(generics.ListCreateAPIView):
+    queryset = models.Order.objects.all()
+    serializer_class = serializers.OrderSerializer
+
+class OrderRetrieveView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Order.objects.all()
+    serializer_class = serializers.OrderSerializer
+
+
