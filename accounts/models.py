dcf8c86a79bfae67108d06af5d5392dd9bc285cb
Nicola Ibrahim <nicola123star4@gmail.com>
2022-10-17 12:08:40 +0300

    Make different changes to accounts models

--- accounts/models.py
+++ accounts/models.py
@@ -15,7 +15,7 @@ class UserManager(BaseUserManager):
     def create(self, **kwargs):
         if not kwargs.get('username'):
             raise ValidationError("Username not given")
-        
+
         user = self.model(**kwargs)
         user.set_password(kwargs.get('password'))
         user.save(using=self._db)
@@ -41,10 +41,7 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractUser):
-
-
     class Role(models.TextChoices):
-        NONE = 'NONE', 'none'
         CUSTOMER = 'CUSTOMER', 'customer'
         STANDARD = 'STD', 'base employee'
         MANAGER = 'MGR' , 'manager'
@@ -52,11 +49,9 @@ class User(AbstractUser):
         PRESIDENT = 'PRES', 'president'
 
     
-    EMAIL_FIELD = "email"
-    USERNAME_FIELD = "username"
-    REQUIRED_FIELDS = ["email"]
+    # EMAIL_FIELD = "email"
+    # USERNAME_FIELD = "username"
     
-    objects = UserManager()
 
 
     phone_number = models.IntegerField(null=True, blank=True)
@@ -64,20 +59,30 @@ class User(AbstractUser):
     city = models.CharField(max_length=50, null=True, blank=True)
     street = models.CharField(max_length=50, null=True, blank=True)
     zipcode = models.IntegerField(null=True, blank=True)
-    role = models.CharField(max_length=25, null=True, blank=True, choices=Role.choices, default=Role.NONE)
+    role = models.CharField(max_length=25, choices=Role.choices, null=False, blank=False)
     manager_id = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL)
+
+    objects = UserManager()
+    # REQUIRED_FIELDS = ["role"]
+
+
+    def __str__(self) -> str:
+        return self.username
+
+
+
 
 class CustomerManager(UserManager):
     def get_queryset(self):
         result = super().get_queryset()
-        return result.filter(role=User.Role.CUSTOMER)
-
+        return result.filter(Q(role=User.Role.CUSTOMER))
+    
 class Customer(User):
     class Meta:
         proxy = True
 
     objects = CustomerManager()
-       
+
     def save(self, *args, **kwargs) -> None:
         if not self.pk:
             self.role = User.Role.CUSTOMER
@@ -91,17 +96,16 @@ class StaffManager(UserManager):
 
 
 class Staff(User):
-
     class Meta:
         proxy = True
 
-    REQUIRED_FIELDS = ['role']
 
     objects = StaffManager()
 
     def save(self, *args, **kwargs) -> None:
         if not self.pk:
             self.is_staff = True
+            self.role = User.Role.STANDARD
         return super().save(*args, **kwargs)
 
 
