0bac388100a154ab589d8649737b447ada7cfbc9
Nicola Ibrahim <nicola123star4@gmail.com>
2022-10-22 12:50:25 +0300

    production app urls

--- production/urls.py
+++ production/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("items/", views.ItemsListView.as_view(), name='items-list'),
+    path("items/<int:pk>", views.ItemRetrieveView.as_view(), name='item-detail'),
 
 ]
