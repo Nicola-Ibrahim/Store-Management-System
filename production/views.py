8f297d051b66ebde6974ede7befb17a908f5a4cd
Nicola Ibrahim <nicola123star4@gmail.com>
2022-10-22 12:50:40 +0300

    production app views

--- production/views.py
+++ production/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Item
+from . import serializers
 
 # Create your views here.
+
+class ItemsListView(generics.ListAPIView):
+    queryset = Item.objects.all()
+    serializer_class = serializers.ItemSerializer
+
+
+class ItemRetrieveView(generics.RetrieveAPIView):
+    queryset = Item.objects.all()
+    serializer_class = serializers.ItemSerializer
+
+
