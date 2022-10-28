4a4a67351793ecd087e5944dff468a07e76b285a
Nicola Ibrahim <nicola123star4@gmail.com>
2022-10-28 12:59:26 +0200

    Adding some changes on order serializer for creating and displaying behavior

--- sales/serializers.py
+++ sales/serializers.py
@@ -1,36 +1,55 @@
 
 from rest_framework import serializers
-from .dataclasses import OrderService, SoldItemDetail
 from .models import Order, OrderItem
 from production.models import Product
 from accounts.models import Customer, Staff
+from accounts.serializers import CustomersSerializer, StaffSerializer
+
+
+
+class OrdersInlineSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    total_price = serializers.IntegerField()
+
 
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
-
-    # Add extra read_only field
-    order_id = serializers.ReadOnlyField(source='order.id')
-    item_name = serializers.ReadOnlyField(source='item.name')
-
-
     class Meta:
         model = OrderItem
-        fields = ['order_id', 'item_name', 'consume_quantity', 'discount']
-
+        fields = '__all__'
 
 class OrderSerializer(serializers.ModelSerializer):
 
     # Add extra read_only field
-    customer_name = serializers.ReadOnlyField(source='customer.username')
-    staff_name = serializers.ReadOnlyField(source='staff.username')
+    url = serializers.HyperlinkedIdentityField(
+        view_name='order-detail',
+        lookup_field='pk',
+        read_only=True
+    )
+
+    purchased_by = CustomersSerializer(source='customer', read_only=True)
+    processed_by = StaffSerializer(source='staff', read_only=True)
+
+    related_orders = OrdersInlineSerializer(source='customer.customer_orders.all', many=True, read_only=True)
 
     # M2M relation field
     items = serializers.SerializerMethodField()
 
     class Meta:
         model = Order
-        fields = ['id', 'url', 'customer_name', 'staff_name', 'customer', 'staff', 'items', 'total_price']
+        fields = [
+            'id', 
+            'url', 
+            'purchased_by', 
+            'processed_by',
+            'related_orders', 
+            'customer', 
+            'staff', 
+            'items', 
+            'total_price'
+            
+        ]
         read_only_fields = ['id']
 
         extra_kwargs = {
@@ -38,24 +57,12 @@ class OrderSerializer(serializers.ModelSerializer):
             'staff': {'write_only': True},
         }    
     
-    def get_items(self, instance):
+    def get_items(self, obj):
         """Custom Method-field to display related items in a specific shape"""
 
         # Retrieve the sold items that relate to order 
-        order_items = OrderItem.objects.filter(order=instance)
-
-        order_items_list = list()
-
-        # Iterate through items
-        for order_item in order_items:
-            order_items_list.append({
-                'item': order_item.item.name,
-                'consume quantity': order_item.consume_quantity, 
-                'discount': order_item.discount,
-                'price' : order_item.price
-            })
-
-        return order_items_list
+        qs = OrderItem.objects.filter(order=obj)
+        return OrderItemSerializer(qs, many=True, context=self.context).data
 
     def to_internal_value(self, data):
         """Change the inserting data from str to num for model"""
@@ -67,21 +74,33 @@ class OrderSerializer(serializers.ModelSerializer):
 
         for item in data.get('items'):
             if(isinstance(item['item'], str)):
-                item['item'] = Product.objects.get(name=item['item'])
+                item['item'] = Product.objects.get(name=item['item']).id
         
         return data
-    
+
 
     def create(self, validated_data):
-        items: list = validated_data.pop('items')
-        items = [SoldItemDetail(**item) for item in items]
+        order_items: list = validated_data.pop('items')
+
+
+        # Check available items
+        for order_item in order_items:
+            item = Product.objects.get(id=order_item['item'])
+            item.is_available(order_item['consume_quantity'])
 
         # Create order instance
         instance = super().create(validated_data)
 
+        for item in order_items:
+            item['order'] = instance.id
+        
         # Link order with inserted items
-        OrderService(order=instance).link(sold_items=items)
+        ser = OrderItemSerializer(data=order_items, many=True)
+        ser.is_valid(raise_exception=True)
+        ser.save()
 
 
         return instance
+
+    
 
